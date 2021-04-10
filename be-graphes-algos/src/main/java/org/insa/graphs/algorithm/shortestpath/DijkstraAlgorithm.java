a7102535731f50e745ab48a87d7af861d109efb8
INSA <insa@localhost.localdomain>
2021-04-10 15:52:17 +0200

    Travail sur le Dijkstra + classe Label

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,11 +1,17 @@
 package org.insa.graphs.algorithm.shortestpath;
 import java.util.ArrayList;
+
+import org.insa.graphs.algorithm.utils.BinaryHeap;
 import org.insa.graphs.model.*;
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
+	protected int nombre_sommets;
+	protected int sommets_visites;
+	
     public DijkstraAlgorithm(ShortestPathData data) {
         super(data);
+        this.sommets_visites = 0; //Nous n'avons pas commencé à visiter des sommmets
     }
 
     @Override
@@ -14,19 +20,71 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
         ShortestPathSolution solution = null;
         // TODO:
         
-        //Connaitre Origine et Destination
+        //DEFINITION DE MON TAS, TABLEAU DE LABEL ET GRAPHE
+        BinaryHeap<Label> mon_tas = new BinaryHeap<Label>();
+        Graph graphe = data.getGraph();
+        Label tableauLabels[] = new Label[graphe.size()];
+        Arc[] arcsPredecesseurs = new Arc[graphe.size()];
+        
+        //CONNAITRE ORIGINE ET DESTINATION
         Node origine = data.getOrigin();
         Node destination = data.getDestination();
-        Graph graph = data.getGraph();
         
-        //INITIALISATION DE TOUS LES LABELS
-        int compteur = 0;
-        ArrayList<Label> labels = new ArrayList<>();
-        for(Node i :graph.getNodes()) {
-        	Label labels[i] = new Label(i);
+        //AJOUT DU DEPART
+        Label depart = newLabel(origine, data);
+        tableauLabels[depart.getCourant().getId()] = depart;
+        mon_tas.insert(depart); //Phase insertion dans le tas
+        depart.setTas(true);
+        depart.setCout(0);
+        
+        boolean cond_arret = false;
+        double cout_algo;
+        while ((!mon_tas.isEmpty()) && (!cond_arret)) { //Tant qu'on a pas traité tous les sommets
+        	
+        	Label noeud_ac = mon_tas.deleteMin(); //On enlève le min du tas
+        	notifyNodeMarked(noeud_ac.getCourant());
+        	noeud_ac.setMarque(true); //On marque ce noeud actuel
+        	
+        	if(noeud_ac.getCourant() == destination) { //On est sur la destination
+        		cout_algo = noeud_ac.getCout();
+        		cond_arret = true;
+        	}
+        	
+        	for(Arc a : noeud_ac.getCourant().getSuccessors()) { //On regarde tous les successeurs
+        		
+        		if(tableauLabels[a.getDestination().getId()] == null) {
+        			notifyDestinationReached(a.getDestination());
+        			tableauLabels[a.getDestination().getId()] = newLabel(a.getDestination(), data);
+        		}
+        		
+        		if(tableauLabels[a.getDestination().getId()].getMarque() == false) {
+        			
+        			if(tableauLabels[a.getDestination().getId()].getCout() > noeud_ac.getCout() + data.getCost(a)) {
+        				tableauLabels[a.getDestination().getId()].setCout(noeud_ac.getCout() + (double) data.getCost(a)); //COUT DE Y = COUT DE X + W(X,Y)
+        				tableauLabels[a.getDestination().getId()].setPere(noeud_ac.getCourant());
+        				
+        				if(tableauLabels[a.getDestination().getId()].getTas()) {
+        					mon_tas.remove(tableauLabels[a.getDestination().getId()]); //MISE A JOUR DANS LE TAS (REMOVE -> INSERTION)
+        				}
+        				else {
+        					tableauLabels[a.getDestination().getId()].setTas(true);
+        				}
+        				
+        				mon_tas.insert(tableauLabels[a.getDestination().getId()]); //INSERTION DE Y DANS TAS
+        				arcsPredecesseurs[a.getDestination().getId()] = a;
+        			}
+        			
+        		}
+        	}
+        	
         }
         
         return solution;
     }
+    
+	//ASSOCIER UN LABEL ET UN NOEUD
+	protected Label newLabel(Node node, ShortestPathData data) {
+		return new Label(node);
+	}
 
 }
--- be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
@@ -1,12 +1,12 @@
 package org.insa.graphs.model;
 
-public class Label {
+public class Label implements Comparable<Label> { //Permettant de mettre des labels dans mon tas
 	
+	protected double cout;
 	private Node courant; 
-	private final Node pere;
-	
+	private Node pere;
 	private boolean marque;
-	private double cout;
+	private boolean appartientTas;
 	
 	//Constructeur de la classe Label
 	public Label (Node courant) {
@@ -14,6 +14,7 @@ public class Label {
 		this.pere = null;
 		this.marque = false;
 		this.cout = 1.0/0.0;     //Je choisis la convention infini pour un noeud ou le cout est inconnu
+		this.appartientTas = false;
 	}
 	
 	//Ensemble de GET
@@ -33,9 +34,43 @@ public class Label {
 		return this.cout;
 	}
 	
+	public boolean getTas() {
+		return this.appartientTas;
+	}
+	
 	//ENSEMBLE DE Set
 	public void setCourant(Node Courant) {
 		this.courant = Courant;
 	}
-
+	
+	public void setPere(Node pere) {
+		this.pere = pere;
+	}
+	
+	public void setTas(boolean b) {
+		this.appartientTas = b;
+	}
+	
+	public void setCout(double c) {
+		this.cout = c;
+	}
+	
+	public void setMarque(boolean b) {
+		this.marque = b;
+	}
+	
+	//Methode qui doit être définie car abstract
+	public int compareTo(Label autre) {
+        int resultat;
+        if(this.getCout() < autre.getCout()) {
+        	resultat = -1;
+        }
+        else if (this.getCout() == autre.getCout()) {
+        	resultat = 0;
+        }
+        else {
+        	resultat = 1;
+        }
+        return resultat;
+	}
 }
